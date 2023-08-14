4bea54198a761ea88a1355d071eb6ed8decd6ada
sakakibara <sakakilabo0000@gmail.com>
2023-08-14 15:34:27 +0900

    t_vectorindex.py is complete

---
+++ tutorial/t_multiqueryretriever.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python3
+# vim:fenc=utf-8
+#
+# Copyright © 2023 sakakibara <sakakibara@dyana>
+#
+# Distributed under terms of the MIT license.
+# Build a sample vectorDB
+
+from langchain.vectorstores import Chroma
+from langchain.document_loaders import WebBaseLoader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chat_models import ChatOpenAI
+from langchain.retrievers.multi_query import MultiQueryRetriever
+
+
+# Load blog post
+loader = WebBaseLoader("https://lilianweng.github.io/posts/2023-06-23-agent/")
+data = loader.load()
+
+# Split
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+splits = text_splitter.split_documents(data)
+
+# VectorDB
+embedding = OpenAIEmbeddings()
+vectordb = Chroma.from_documents(documents=splits, embedding=embedding)
+
+# Multi Retriever
+question = "What are the approaches to Task Decomposition?"
+llm = ChatOpenAI(temperature=0)
+retriever_from_llm = MultiQueryRetriever.from_llm(
+    retriever=vectordb.as_retriever(), llm=llm
+)
