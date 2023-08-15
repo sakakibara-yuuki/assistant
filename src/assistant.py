e9aad0d5f876c10d751408305990ae5820799452
sakakibara <sakakilabo0000@gmail.com>
2023-08-16 00:47:49 +0900

    Delete chat model llm

--- src/assistant.py
+++ src/assistant.py
@@ -7,7 +7,13 @@ from langchain.document_loaders import (
     BSHTMLLoader,
     WebBaseLoader,
     PyPDFLoader,
+    UnstructuredPowerPointLoader,
+    UnstructuredMarkdownLoader,
 )
+from langchain.document_loaders.recursive_url_loader import RecursiveUrlLoader
+from langchain.document_loaders.generic import GenericLoader
+from langchain.document_loaders.parsers import LanguageParser
+from langchain.text_splitter import Language
 from langchain.text_splitter import (
     CharacterTextSplitter,
     RecursiveCharacterTextSplitter,
@@ -17,8 +23,12 @@ from langchain.vectorstores import Chroma
 from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
+from pydantic import BaseModel
+from bs4 import BeautifulSoup
 import click
 import logging
+import esprima
+import pprint
 
 # -[x]: input list
 # -[x]: click
@@ -35,7 +45,8 @@ logger = logging.getLogger(__name__)
 
 
 class BookShelf:
-    def __init__(self, input_list: list = None):
+    def __init__(self, input_list:list = None):
+
         self.embedding = OpenAIEmbeddings()
 
         if input_list is not None:
@@ -54,24 +65,33 @@ class BookShelf:
                 if o.scheme not in ("http", "https"):
                     raise Exception(f"{uri} cannot be found, and this is not URL")
                     continue
-                logger.info(f"::::::::::::LOAD:::::::::::::  {uri}")
                 loader = WebBaseLoader(uri)
+                # loader = RecursiveUrlLoader(url=uri, max_depth=1, extractor=lambda x: BeautifulSoup(x, "html.parser").text)
                 loaders.append(loader)
                 continue
 
             suffix = p.suffix
-            if suffix == None:
-                loader = TextLoader(uri)
-            elif suffix == ".text":
+            if suffix == ".txt":
                 loader = TextLoader(uri)
             elif suffix == ".html":
-                loader = BSHTMLLoader(uri)
-            elif suffix == ".md":
-                loader = TextLoader(uri)
-            elif suffix == ".pdf":
-                loader = PyPDFLoader(uri)
+                # loader = BSHTMLLoader(uri)
+                loader = UnstructuredHTMLLoader(uri)
+            # elif suffix == ".md":
+            #     loader = UnstructuredMarkdownLoader(uri)
+            # elif suffix == ".pdf":
+            #     loader = PyPDFLoader(uri)
+            # elif suffix == ".pptx":
+            #     loader = UnstructuredPowerPointLoader(uri)
+            # elif suffix == ".py":
+            #     loader = GenericLoader.from_filesystem(
+            #         uri,
+            #         glob="*",
+            #         suffixes=[".py"],
+            #         parser=LanguageParser(language=Language.PYTHON, parser_threshold=400),
+            #     )
+            else:
+                continue
 
-            logger.info(f"::::::::::::LOAD:::::::::::::  {uri}")
             loaders.append(loader)
 
         docs = []
@@ -127,21 +147,35 @@ class BookShelf:
         exists=True, file_okay=False, dir_okay=True, writable=False, readable=True
     ),
 )
-def cli(prompt, answer, reference):
+@click.option(
+    "-l",
+    "--link",
+    default="./links",
+    show_default=True,
+    type=click.Path(
+        exists=True, file_okay=True, dir_okay=False, writable=False, readable=True
+    ),
+)
+def cli(prompt, answer, reference, link):
     prompt_path = pathlib.Path(prompt)
     answer_path = pathlib.Path(answer) / prompt_path.name
     reference_path = pathlib.Path(reference)
-    input_list = [str(p) for p in reference_path.glob("./*")]
-    input_list.append("https://onelab.info/")
+    input_list = [str(p) for p in reference_path.glob("**/*") if (not p.is_dir()) and (p.suffix == ".py")]
+    with open(link, 'r') as f:
+        lines = f.readlines()
+    for line in lines:
+        input_list.append(line.rstrip())
     main(prompt_path, answer_path, reference_path, input_list)
 
-
 def main(prompt_path, answer_path, reference_path, input_list):
-    bookshelf = BookShelf(input_list=input_list)
+    # bookshelf = BookShelf(input_list=input_list)
+    bookshelf = BookShelf()
     vectordb = bookshelf.vectordb
 
     # LLM ラッパーの初期化
-    llm = ChatOpenAI(model_name="gpt-4", temperature=0.9, max_tokens=500)
+    # llm = ChatOpenAI(model_name="gpt-4", temperature=1.0, max_tokens=500)
+    # chat modelだとうまくいかない
+    llm = OpenAI()
 
     qa = RetrievalQA.from_llm(llm=llm, retriever=vectordb.as_retriever())
 
