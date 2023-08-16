8d60ec4b27d2e9fcfcd0778ebfa70dc7d05f2b4a
sakakibara <sakakilabo0000@gmail.com>
2023-08-16 22:36:42 +0900

    ADD ui.py

---
+++ src/ui.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python3
+# vim:fenc=utf-8
+#
+# Copyright © 2023 sakakibara <sakakibara@dyana>
+#
+# Distributed under terms of the MIT license.
+
+import streamlit as st
+import pandas as pd
+from io import StringIO
+
+from assistant import BookShelf
+
+
+bookshelf = BookShelf()
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    st.write(bytes_data)
+
+    # To convert to a string based IO:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    st.write(stringio)
+
+    # To read file as string:
+    string_data = stringio.read()
+    st.write(string_data)
+
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
