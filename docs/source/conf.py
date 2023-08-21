8cddba7013f70e04c19b1589495205a035c56771
sakakibara <sakakilabo0000@gmail.com>
2023-08-21 14:27:00 +0900

    change theme of doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,8 @@ language = 'ja'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
+# html_theme = 'furo'
+html_theme = 'alabaster'
 html_static_path = ['_static']
 
 html_css_files = [
