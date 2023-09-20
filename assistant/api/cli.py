b4e008173e90613f578103f2a46c9b9cf8505c71
sakakibara <sakakilabo0000@gmail.com>
2023-09-20 23:58:57 +0900

    feat: create in api/cli.py design of assistant for you

--- assistant/api/cli.py
+++ assistant/api/cli.py
@@ -4,4 +4,18 @@
 # Copyright © 2023 sakakibara <sakakibara@dyana>
 #
 # Distributed under terms of the MIT license.
+"""
+cliは基本的に
 
+python assistant create bookshelf
+のように英語の5文型を基本とする
+
+python assistant create bookshelf
+python assistant show bookshelf
+python assistant summary --about book1
+python assistant add book
+python assistant chat --about book1 book2
+python assistant delete book1
+python assistant fill template.md --about book
+--aboutの後にはbookを入れる.
+"""
