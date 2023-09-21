f18c7ff540ca12217ff10c4b18cfa8c73eaa9fc1
sakakibara <sakakilabo0000@gmail.com>
2023-09-21 23:56:21 +0900

    feat: add cli template

--- assistant/api/cli.py
+++ assistant/api/cli.py
@@ -19,3 +19,99 @@ python assistant delete book1
 python assistant fill template.md --about book
 --aboutの後にはbookを入れる.
 """
+import click
+
+
+@click.group()
+@click.pass_context
+def cli(ctx):
+    pass
+
+
+@cli.command()
+@click.pass_context
+@click.argument('bookshelf')
+def create(ctx, bookshelf):
+    """
+    python assistant create bookshelf
+    """
+    pass
+
+
+@cli.command()
+@click.pass_context
+@click.argument('bookshelf')
+def show(ctx, bookshelf):
+    """
+    python assistant show bookshelf
+    """
+    pass
+
+
+@cli.command()
+@click.pass_context
+@click.option(
+    "-a",
+    "--about",
+    default=None,
+    show_default=True,
+    nargs=2,
+)
+def summary(ctx, about):
+    """
+    python assistant summary --about book1
+    """
+    book1, book2 = about
+    pass
+
+
+@cli.command()
+@click.pass_context
+@click.argument('book')
+def add(ctx, book):
+    """
+    python assistant add book
+    """
+    pass
+
+
+@cli.command()
+@click.pass_context
+@click.option(
+    "-a",
+    "--about",
+    default=None,
+    show_default=True,
+    nargs=2,
+)
+def chat(ctx, book1, book2):
+    """
+    python assistant chat --about book1 book2
+    """
+    book1, book2 = about
+    pass
+
+
+@cli.command()
+@click.pass_context
+@click.argument('book')
+def delete(ctx, book):
+    """
+    python assistant delete book
+    """
+    pass
+
+
+@cli.command()
+@click.pass_context
+@click.argument('template')
+@click.option(
+    "--about",
+    default=None,
+    show_default=True,
+)
+def fill(ctx, template, about):
+    """
+    python assistant fill template.md --about book
+    """
+    pass
